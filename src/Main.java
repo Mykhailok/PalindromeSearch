f99bca4bb82e8da038b8d238ef65c4049d4bc3fc
Mykhailok <kosinskiy.mihael@gmail.com>
2017-01-17 12:51:22 +0200

    right result

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by MihaelKO on 16.01.2017. start - 12.45, end 16.10.
  */
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        int a = 100, b = 999; //Initialize two numbers, first is min of 3-digit numbers, second is max
-        List<String> resultList = new ArrayList();//Initialize list where our palindroms we'll be stored
-        //List<Integer> resultList = new ArrayList();//Line with initialization of integer List
-        while (a <= b) { //Start cycle
-            int c = a * b; //get multiplication og two numbers
-            a++; // make all numbers are been multiplicated
-            isPalindrome(resultList, c);//check if the current number palindrom or not, and if it so, wright it to List
+        int a = 100, b = 1000; //Initialize two numbers, first is min of 3-digit numbers, second is max
+        TreeSet<Integer> resultList = new TreeSet();//Line with initialization of integer List
+        for (int i = a; i < b; i++) { //Start cycle
+            for (int j = a; j < b; j++){
+                int c = i * j; //get multiplication of two numbers
+                isPalindrome(resultList, c);//check if the current number palindrom or not, and if it so, wright it to List
+            }
         }
         try{
             System.out.println("The largest palindrome made from the product of two numbers is: "
-                    + resultList.get(resultList.size()-1)); //Print text if List have palindrom. Print only max palindrom
+                    + resultList.last()); //Print text if List have palindrom. Print only max palindrom
         }catch (ArrayIndexOutOfBoundsException e){ //catch exception if there is no palindroms at all and the List is Empty
             System.out.println("There is no largest palindrome made from the product of two 3-digit numbers.");
         }
     }
 
     @SuppressWarnings("unchecked")
-    private static void isPalindrome(List resultList, int c) { //method for checking palindrom
+    private static void isPalindrome(Set<Integer> resultList, int c) { //method for checking palindrom
         String string = String.valueOf(c); // converting number to string
         char[] myCharArray = string.toCharArray(); //split string to array of chars
         char[] myTempArray = new char[myCharArray.length];//make temporary empty array with lenght equals of char array
@@ -35,14 +35,9 @@ public class Main {
                 reverse += myTempArray[j];//start filling reversed string
             }
         }
-       /* In case if we need integers uncomment this code and line with Integer List
-        int num = Integer.parseInt(reverse);
+        int num = Integer.parseInt(reverse); //convert to Integer
         if (c == num) { //check if strings are equal
             resultList.add(c);//and if equal add this number to List of palindroms
-        }
-        */
-        if (string.equals(reverse)) { //check if strings are equal
-                resultList.add(string);//and if equal add this number to List of palindroms
         }
     }
 }
